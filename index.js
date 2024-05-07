e737f1246d6e608a916215de31a1a31c3bd6feff
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2024-05-08 01:42:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const validator = require('validator');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 5000;
 const { v4: uuidv4 } = require('uuid');
 const { Client, Account, Databases, ID, Query, Storage } = require('appwrite');
 
@@ -92,12 +92,12 @@ app.get('/overview', (req, res) => {
   res.sendFile(path.join(initial_path, "overview.html"));
 });
 
-app.get('/paypal', (req, res) => {
-  res.sendFile(path.join(initial_path, "paypal.html"));
-});
-
 app.get('/template', (req, res) => {
   res.sendFile(path.join(initial_path, "template.html"));
+});
+
+app.get('/paypal', (req, res) => {
+  res.sendFile(path.join(initial_path, "paypal.html"));
 });
 
 app.get('/sign-up', (req, res) => {
@@ -165,7 +165,7 @@ app.post('/add-product', (req, res) => {
             name: req.body.name,
             price: req.body.price.toString(),
             description: req.body.description,
-            image: 'https://fullapp-861f.onrender.com/uploads/' + filenames[0],
+            image: 'http://localhost:5000/uploads/' + filenames[0],
             size1: req.body.size,
             size2: req.body.size2,
             size3: req.body.size3,
@@ -173,9 +173,9 @@ app.post('/add-product', (req, res) => {
             status: req.body.status1,
             delivery: req.body.delivery,
             store: req.body.shop,
-            image1: 'https://fullapp-861f.onrender.com/additional/' + filenames[1],
-            image2: 'https://fullapp-861f.onrender.com/additional/' + filenames[2],
-            image3: 'https://fullapp-861f.onrender.com/additional/' + filenames[3],
+            image1: 'http://localhost:5000/additional/' + filenames[1],
+            image2: 'http://localhost:5000/additional/' + filenames[2],
+            image3: 'http://localhost:5000/additional/' + filenames[3],
              
             // ...
           }
@@ -716,9 +716,11 @@ app.post('/onboard', (req, res) => {
           externalUUID, // Utilise l'UUID externe comme identifiant du document
           { 
           documentId: externalUUID,
-          logo: 'https://fullapp-861f.onrender.com/logo/'+ filenames[0],
+          logo: 'http://localhost:5000/logo/'+ filenames[0],
           store: req.body.store,
           owner: req.body.owner,
+          client: req.body.client,
+          secret: req.body.secret,
           userId: userIdGlobal,
           } // Convertit formData en chaîne de caractères
         );
@@ -762,15 +764,22 @@ app.get('/dashboard', (req, res) => {
     const shopId = req.query.shop; // Récupère l'ID de la boutique depuis la requête
     console.log(shopId); // Vérifiez si l'ID de la boutique est correctement récupéré
 
-    const promise = databases.listDocuments('selllab-database-2024', 'visitor-2024');
-    promise
-      .then(function (response) {
-        console.log(response); // Affiche la réponse complète dans la console
+    const promise1 = databases.listDocuments('selllab-database-2024', 'visitor-2024');
+    const promise2 = databases.listDocuments('selllab-database-2024', 'store-creation-2024');
 
-        // Filtrer les documents en fonction de l'ID utilisateur et de l'ID de la boutique
-        const filteredDocuments = response.documents.filter(
-          (document) => document.shopId === shopId
-        );
+    Promise.all([promise1, promise2])
+      .then(function (responses) {
+        const visitorResponse = responses[0];
+        const storeCreationResponse = responses[1];
+
+        console.log(visitorResponse); // Affiche la réponse complète de visitor-2024 dans la console
+        console.log(storeCreationResponse); // Affiche la réponse complète de store-creation-2024 dans la console
+
+        // Filtrer les documents de visitor-2024 en fonction de l'ID de la boutique
+        const filteredDocuments = visitorResponse.documents.filter((document) => {
+          return document.shopId === shopId;
+        });
+
         console.log(filteredDocuments); // Vérifiez les documents filtrés
 
         let totals = {
@@ -789,25 +798,56 @@ app.get('/dashboard', (req, res) => {
           totals.visits += document.visits;
         }
 
+        // Récupérer le logo à partir de la réponse storeCreationResponse
+        const store = storeCreationResponse.documents.find((doc) => doc.documentId === shopId);
+        const logo = store ? store.logo : '';
 
-        // Générer le code HTML correspondant aux documents filtrés
+        const name = storeCreationResponse.documents.find((doc) => doc.documentId === shopId);
+        const shopname = name ? name.store : '';
+        // Continuez le reste de votre code ici
+        
+
         let html = `
           <!DOCTYPE html>
           <html>
           <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta name="theme-color" content="#db1a5a">
           <link rel="stylesheet" href="style-component/app.css">
           <link rel="stylesheet" href="style-component/overview.css">
           <link rel="stylesheet" href="style-component/manager.css">
+          <link rel="stylesheet" href="style-component/orders.css">
           <link rel="shortcut icon" href="favicon/favicon.svg" type="image/x-icon">
           <title>store insights</title>
           </head>
-          <body class="dark">
+          <body class="dar">
 
           <div class="main-container">
           <aside class="aside-container">
+          <div class="menu-box">
+                      <a href="/overview" class="list-menu-box menu-active">
+                          <img src="${logo}" alt="" class="overview-store-image">
+                          <span>${shopname}</span>
+                      </a>
+                  </div>
+                  <div class="menu-box">
+                    <a href="/orders?shop=${shopId}" class="list-menu-box">
+                          <svg aria-hidden="true" height="13" viewBox="0 0 16 16" version="1.1" width="13" data-view-component="true"><path d="M2.8 2.06A1.75 1.75 0 0 1 4.41 1h7.18c.7 0 1.333.417 1.61 1.06l2.74 6.395c.04.093.06.194.06.295v4.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25v-4.5c0-.101.02-.202.06-.295Zm1.61.44a.25.25 0 0 0-.23.152L1.887 8H4.75a.75.75 0 0 1 .6.3L6.625 10h2.75l1.275-1.7a.75.75 0 0 1 .6-.3h2.863L11.82 2.652a.25.25 0 0 0-.23-.152Zm10.09 7h-2.875l-1.275 1.7a.75.75 0 0 1-.6.3h-3.5a.75.75 0 0 1-.6-.3L4.375 9.5H1.5v3.75c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25Z"></path></svg>
+                          <span>All orders</span>
+                      </a>
+                  </div>
+                  <div class="menu-box">
+                      <a href="/add-product?shop=${shopId}" class="list-menu-box">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M11.707 2.293A.997.997 0 0 0 11 2H6a.997.997 0 0 0-.707.293l-3 3A.996.996 0 0 0 2 6v5c0 .266.105.52.293.707l10 10a.997.997 0 0 0 1.414 0l8-8a.999.999 0 0 0 0-1.414l-10-10zM13 19.586l-9-9V6.414L6.414 4h4.172l9 9L13 19.586z"/><circle cx="8.353" cy="8.353" r="1.647"/></svg>
+                          <span>Add product</span>
+                      </a>
+                  </div>
+                  <div class="menu-box">
+                      <a href="/builder?shop=${shopId}" class="list-menu-box">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M22 5c0-1.654-1.346-3-3-3H5C3.346 2 2 3.346 2 5v2.831c0 1.053.382 2.01 1 2.746V19c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-8.424c.618-.735 1-1.692 1-2.746V5zm-2 0v2.831c0 1.14-.849 2.112-1.891 2.167L18 10c-1.103 0-2-.897-2-2V4h3c.552 0 1 .449 1 1zM10 4h4v4c0 1.103-.897 2-2 2s-2-.897-2-2V4zM4 5c0-.551.448-1 1-1h3v4c0 1.103-.897 2-2 2l-.109-.003C4.849 9.943 4 8.971 4 7.831V5zm6 14v-3h4v3h-4zm6 0v-3c0-1.103-.897-2-2-2h-4c-1.103 0-2 .897-2 2v3H5v-7.131c.254.067.517.111.787.125A3.988 3.988 0 0 0 9 10.643c.733.832 1.807 1.357 3 1.357s2.267-.525 3-1.357a3.988 3.988 0 0 0 3.213 1.351c.271-.014.533-.058.787-.125V19h-3z"/></svg>
+                          <span>Builder store</span>
+                      </a>
+                  </div>
               <div class="menu-box">
                   <a href="/onboard" class="list-menu-box active">
                       <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="17"><path d="M160-720v-80h640v80H160Zm0 560v-240h-40v-80l40-200h640l40 200v80h-40v240h-80v-240H560v240H160Zm80-80h240v-160H240v160Zm-38-240h556-556Zm0 0h556l-24-120H226l-24 120Z"/></svg>
@@ -815,7 +855,7 @@ app.get('/dashboard', (req, res) => {
                   </a>
               </div>
               <div class="menu-box">
-                  <a href="/onboard" class="list-menu-box">
+                  <a href="/account-settings" class="list-menu-box">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 2c3.213 0 5.982 1.908 7.254 4.648a7.8 7.8 0 0 1-.895-.498c-.409-.258-.873-.551-1.46-.772-.669-.255-1.4-.378-2.234-.378s-1.565.123-2.234.377c-.587.223-1.051.516-1.472.781-.378.237-.703.443-1.103.594C9.41 8.921 8.926 9 8.33 9c-.595 0-1.079-.079-1.524-.248-.4-.151-.728-.358-1.106-.598-.161-.101-.34-.208-.52-.313C6.587 5.542 9.113 4 12 4zm0 16c-4.411 0-8-3.589-8-8 0-.81.123-1.59.348-2.327.094.058.185.11.283.173.411.26.876.554 1.466.776.669.255 1.399.378 2.233.378.833 0 1.564-.123 2.235-.377.587-.223 1.051-.516 1.472-.781.378-.237.703-.443 1.103-.595.445-.168.929-.247 1.525-.247s1.08.079 1.525.248c.399.15.725.356 1.114.602.409.258.873.551 1.46.773.363.138.748.229 1.153.291.049.357.083.717.083 1.086 0 4.411-3.589 8-8 8z"/><circle cx="8.5" cy="13.5" r="1.5"/><circle cx="15.5" cy="13.5" r="1.5"/></svg>
                       <span>Account settings</span>
                   </a>
@@ -827,7 +867,7 @@ app.get('/dashboard', (req, res) => {
                   </a>
               </div>
               <div class="menu-box">
-                  <a href="/onboard" class="list-menu-box">
+                  <a href="/selllab-settings" class="list-menu-box">
                       <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24"><path d="M12.01 2.25c.74 0 1.47.1 2.18.25.32.07.55.33.59.65l.17 1.53a1.38 1.38 0 001.92 1.11l1.4-.61c.3-.13.64-.06.85.17a9.8 9.8 0 012.2 3.8c.1.3 0 .63-.26.82l-1.24.92a1.38 1.38 0 000 2.22l1.24.92c.26.19.36.52.27.82a9.8 9.8 0 01-2.2 3.8.75.75 0 01-.85.17l-1.4-.62a1.38 1.38 0 00-1.93 1.12l-.17 1.52a.75.75 0 01-.58.65 9.52 9.52 0 01-4.4 0 .75.75 0 01-.57-.65l-.17-1.52a1.38 1.38 0 00-1.93-1.11l-1.4.62a.75.75 0 01-.85-.18 9.8 9.8 0 01-2.2-3.8c-.1-.3 0-.63.27-.82l1.24-.92a1.38 1.38 0 000-2.22l-1.24-.92a.75.75 0 01-.28-.82 9.8 9.8 0 012.2-3.8c.23-.23.57-.3.86-.17l1.4.62c.4.17.86.15 1.25-.08.38-.22.63-.6.68-1.04l.17-1.53a.75.75 0 01.58-.65c.72-.16 1.45-.24 2.2-.25zm0 1.5c-.45 0-.9.04-1.35.12l-.11.97a2.89 2.89 0 01-4.02 2.33l-.9-.4A8.3 8.3 0 004.28 9.1l.8.59a2.88 2.88 0 010 4.64l-.8.59a8.3 8.3 0 001.35 2.32l.9-.4a2.88 2.88 0 014.02 2.32l.1.99c.9.15 1.8.15 2.7 0l.1-.99a2.88 2.88 0 014.02-2.32l.9.4a8.3 8.3 0 001.36-2.32l-.8-.59a2.88 2.88 0 010-4.64l.8-.59a8.3 8.3 0 00-1.35-2.32l-.9.4a2.88 2.88 0 01-4.02-2.32l-.11-.98c-.45-.08-.9-.11-1.34-.12zM12 8.25a3.75 3.75 0 110 7.5 3.75 3.75 0 010-7.5zm0 1.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"></path></svg>
                       <span>Selllab settings</span>
                   </a>
@@ -882,39 +922,12 @@ app.get('/dashboard', (req, res) => {
                   </div>
                  
               </div>
-              <aside class="main-container-aside">
-                  <div class="menu-box">
-                      <a href="/overview" class="list-menu-box menu-active">
-                          <img src="logo/nike-logo-removebg-preview (1).png" alt="" class="overview-store-image">
-                          <span>Sparkle inc.</span>
-                      </a>
-                  </div>
-                  <div class="menu-box">
-                      <a href="/orders" class="list-menu-box">
-                          <svg aria-hidden="true" height="13" viewBox="0 0 16 16" version="1.1" width="13" data-view-component="true"><path d="M2.8 2.06A1.75 1.75 0 0 1 4.41 1h7.18c.7 0 1.333.417 1.61 1.06l2.74 6.395c.04.093.06.194.06.295v4.5A1.75 1.75 0 0 1 14.25 15H1.75A1.75 1.75 0 0 1 0 13.25v-4.5c0-.101.02-.202.06-.295Zm1.61.44a.25.25 0 0 0-.23.152L1.887 8H4.75a.75.75 0 0 1 .6.3L6.625 10h2.75l1.275-1.7a.75.75 0 0 1 .6-.3h2.863L11.82 2.652a.25.25 0 0 0-.23-.152Zm10.09 7h-2.875l-1.275 1.7a.75.75 0 0 1-.6.3h-3.5a.75.75 0 0 1-.6-.3L4.375 9.5H1.5v3.75c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25Z"></path></svg>
-                          <span>All orders</span>
-                      </a>
-                  </div>
-                  <div class="menu-box">
-                      <a href="/add-product" class="list-menu-box">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M11.707 2.293A.997.997 0 0 0 11 2H6a.997.997 0 0 0-.707.293l-3 3A.996.996 0 0 0 2 6v5c0 .266.105.52.293.707l10 10a.997.997 0 0 0 1.414 0l8-8a.999.999 0 0 0 0-1.414l-10-10zM13 19.586l-9-9V6.414L6.414 4h4.172l9 9L13 19.586z"/><circle cx="8.353" cy="8.353" r="1.647"/></svg>
-                          <span>Add product</span>
-                      </a>
-                  </div>
-                  <div class="menu-box">
-                      <a href="/builder" class="list-menu-box">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M22 5c0-1.654-1.346-3-3-3H5C3.346 2 2 3.346 2 5v2.831c0 1.053.382 2.01 1 2.746V19c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-8.424c.618-.735 1-1.692 1-2.746V5zm-2 0v2.831c0 1.14-.849 2.112-1.891 2.167L18 10c-1.103 0-2-.897-2-2V4h3c.552 0 1 .449 1 1zM10 4h4v4c0 1.103-.897 2-2 2s-2-.897-2-2V4zM4 5c0-.551.448-1 1-1h3v4c0 1.103-.897 2-2 2l-.109-.003C4.849 9.943 4 8.971 4 7.831V5zm6 14v-3h4v3h-4zm6 0v-3c0-1.103-.897-2-2-2h-4c-1.103 0-2 .897-2 2v3H5v-7.131c.254.067.517.111.787.125A3.988 3.988 0 0 0 9 10.643c.733.832 1.807 1.357 3 1.357s2.267-.525 3-1.357a3.988 3.988 0 0 0 3.213 1.351c.271-.014.533-.058.787-.125V19h-3z"/></svg>
-                          <span>Builder store</span>
-                      </a>
-                  </div>
-              </aside>
+              
           </div>
       </div>
             
           </body>
           </html>`;
-
-
 
         // Envoyer le code HTML généré à la vue pour l'affichage
         res.send(html);
@@ -979,7 +992,6 @@ app.get('/items', (req, res) => {
 function generateHTML(documents, step) {
   let html = '<html>';
   html += '<head>';
-  
   html += '<link rel="stylesheet" href="style-component/app.css">';
     html += '<link rel="stylesheet" href="style-component/add-product.css">';
     html += '<link rel="stylesheet" href="style-component/overview.css">';
@@ -1074,7 +1086,6 @@ function generateDocumentFilterHtml(documents) {
   let html = '';
   html += '<html lang="en">';
     html += '<meta charset="UTF-8">';
-  
     html += '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
     html += '<link rel="stylesheet" href="style-component/app.css">';
     html += '<link rel="stylesheet" href="style-component/add-product.css">';
@@ -1211,6 +1222,18 @@ function generateDocumentFilterHtml(documents) {
     html += '<div class="input-group">';
     html += '<input type="text" name="domain" required>';
     html += '<label for="name">Store domain</label>';
+    html += '</div>';
+    html += '<div class="input-group">';
+    html += '<input type="text" name="domain" required>';
+    html += '<label for="name">Store domain</label>';
+    html += '</div>';
+    html += '<div class="input-group">';
+    html += '<input type="text" name="client" required>';
+    html += '<label for="name">Paypal_client</label>';
+    html += '</div>';
+    html += '<div class="input-group">';
+    html += '<input type="text" name="secret" required>';
+    html += '<label for="name">Paypal_secret</label>';
     html += '</div>';
     html += '<button type="submit"  class="branded-button">Create store</button>';
     html += `
@@ -1372,7 +1395,6 @@ app.get('/store', (req, res) => {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                
                 <link rel="stylesheet" href="style-component/app.css">
                 <link rel="stylesheet" href="style-component/template.css">
                 <link rel="shortcut icon" href="logo/nike-logo-removebg-preview (1).png" type="image/x-icon">
@@ -1409,24 +1431,29 @@ app.get('/store', (req, res) => {
                 </div>
                 <div class="product-container">
                   <div class="product">
-                    ${filteredDocuments.map((document) => {
-                      const image = document.image; // Récupérer l'image du document
-                      // Récupérer le documentId du document trouvé
-                      const name = document.name;
-                      const price = document.price;
-                      return `
-                      
-                        <img src="${image}" alt="">
+                  ${filteredDocuments.map((document) => {
+                    const image = document.image; // Récupérer l'image du document
+                    const name = document.name;
+                    const price = document.price;
+                    
+            
+                    return `
+                      <img src="${image}" alt="">
                       <span class="name">${name}</span>
                       <span class="price">$${price},00</span>
-                      `;
-                    }).join('')}
+            
+                      
+                    `;
+                  }).join('')}
                   </div>
                 </div>
                 <footer>
                     <span>Powered by selllab.</span>
                     <span>© copyright 2024 selllab.</span>
                 </footer>
+                
+                  
+                   
               </body>`;
             break;
           case 'theme2':
@@ -1450,7 +1477,6 @@ app.get('/store', (req, res) => {
 });
 
 
-
-
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`le serveur est démarrer au port ${port} `)
+})
